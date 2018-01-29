35128dd4ac03ceef10aaaa2f7d1a55125efd7c40
vatsjay <vatsjay759@gmail.com>
2018-01-29 20:44:00 +0530

    Add files via upload

---
+++ ValueConverters/ApplicationPageValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace ChatApplication
+{
+    public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.Login:
+                    return new LogIn();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
