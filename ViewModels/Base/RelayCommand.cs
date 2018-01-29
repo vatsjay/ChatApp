3255d756bea89d0c2a0f3d943721949e75690bb1
vatsjay <vatsjay759@gmail.com>
2018-01-29 20:41:23 +0530

    Add files via upload

---
+++ ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ChatApplication
+{
+    class RelayCommand : ICommand
+    {
+        //Event raised when CanExecute Changes
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        //variable to store action
+        private Action thisAction;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="action"></param>
+        public RelayCommand(Action action)
+        {
+            thisAction = action;
+        }
+
+        /// <summary>
+        /// Set true for now 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Perfom Action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            thisAction();
+        }
+    }
+}
